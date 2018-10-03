f6565696e99f0fc45694fbdf58e61d51cf50a707
ritsu_kog <twakeswiess@gmail.com>
2018-10-03 22:54:34 +0900

    class.phpを更新

--- class.php
+++ class.php
@@ -23,4 +23,33 @@ class Employee {
 $yamada = new Employee();
 $yamada -> work();    //メソッドの呼び出しにはアロー演算子を用いる
 
+// プロパティ
+
+/*
+
+・プロパティはクラスの中に保持している変数でクラスの定義時に同時に定義する
+・プロパティはアクセス修飾子のあとに変数名をつけて定義する
+・必要があれば続けてデフォルトの値を指定することができる
+
+*/
+
+class Employee {
+
+      public $name;    // 従業員の名前を表すプロパティ
+      public $state = '働いている';   // 従業員の状態を表すプロパティ「デフォルト指定」
+
+      public function work () {
+
+        echo '書籍を整理しています', PHP_EOL;
+
+      }
+}
+
+$yamada = new Employee();
+$yamada -> name = '山田';
+
+    echo $yamada -> state, $yamada -> name, 'さん';    //働いている山田さん
+
+    $yamada -> work();    // 書籍を整理しています
+
 ?>
